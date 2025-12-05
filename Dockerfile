5af3703d9cd2265329535226751b37a77bf68763
AgusFassola <agusfassolacarp@gmail.com>
2025-12-05 20:53:16 -0300

    compose, dockerfile y github actions

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+#imagen base
+ FROM node:16
+#Establecer directorio de trabajo
+ WORKDIR /app
+#Copiar archivos de package.json y package-lock.json
+ COPY package*.json ./
+ RUN npm install
+#Copiar codigo de aplicacion
+ COPY . .
+#Exponer el puerto en el que la aplicacion correra
+ EXPOSE 3000
+#iniciar la aplicacion
+ CMD ["node", "server.js"]
---
+++ index.js
@@ -0,0 +1,16 @@
+//importamos express
+const express = require('express')
+
+//creamos la app
+const app = express();
+
+//Definimos una ruta GET
+app.get('/', (req, res) => {
+    res.json({  message: 'Hola Mundo desde Express'})
+});
+
+//iniciamos el servidor
+const PORT=3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
